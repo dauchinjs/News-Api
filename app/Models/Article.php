82ea70c2a2026564f0a684a5d97edaea4fca60b9
Davids Jansons <93677423+dauchinjs@users.noreply.github.com>
2022-11-30 09:53:09 +0200

    Update Article.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,17 +4,33 @@ namespace App\Models;
 
 class Article
 {
+    private ?string $author;
     private string $title;
     private string $url;
     private string $description;
+    private string $publishedAt;
     private ?string $picture;
 
-    public function __construct(string $title, string $url, string $description, ?string $picture = null)
+    public function __construct(
+        ?string $author,
+        string  $title,
+        string  $url,
+        string  $description,
+        string  $publishedAt,
+        ?string $picture = null
+    )
     {
+        $this->author = $author;
         $this->title = $title;
         $this->url = $url;
         $this->description = $description;
+        $this->publishedAt = $publishedAt;
         $this->picture = $picture;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
     }
 
     public function getTitle(): string
@@ -30,6 +46,11 @@ class Article
     public function getDescription(): string
     {
         return $this->description;
+    }
+
+    public function getPublishedAt(): string
+    {
+        return $this->publishedAt;
     }
 
     public function getPicture(): ?string

9614e74e3b155bf65941b9775c3ea7ab6c73fb7f
Davids Jansons <93677423+dauchinjs@users.noreply.github.com>
2022-12-07 10:23:09 +0200

    Update LoginService.php

--- app/Services/Login/LoginService.php
+++ app/Services/Login/LoginService.php
@@ -3,25 +3,18 @@
 namespace App\Services\Login;
 
 use App\Services\Database;
-use Doctrine\DBAL\Connection;
 
 class LoginService
 {
-    private Connection $connection;
-
-    public function __construct() {
-        $db = new Database();
-        $this->connection = $db->connect();
-    }
-
-    public function execute(LoginServiceRequest $request) {
-        $result = $this->connection->executeQuery(
+    public function execute(LoginServiceRequest $request)
+    {
+        $result = Database::getConnection()->executeQuery(
             'SELECT id FROM `news-api`.users WHERE email=?', [
             $request->getEmail()]);
 
         $id = $result->fetchAssociative();
 
-        $result = $this->connection->executeQuery(
+        $result = Database::getConnection()->executeQuery(
             'SELECT password FROM `news-api`.users WHERE id=?', [$id['id']]);
         $hash = $result->fetchAllAssociative();
 

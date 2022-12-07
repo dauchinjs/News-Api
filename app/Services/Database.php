4c952116f12bf5222eb53c06aab2465d1416ad23
Davids Jansons <93677423+dauchinjs@users.noreply.github.com>
2022-12-07 10:24:18 +0200

    Update Database.php

--- app/Services/Database.php
+++ app/Services/Database.php
@@ -2,20 +2,25 @@
 
 namespace App\Services;
 
-use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Connection;
 
 class Database
 {
-    public function connect()
-    {
-        $connectionParams = [
-            'dbname' => 'news-api',
-            'user' => 'root',
-            'password' => $_ENV['MYSQL_PASSWORD'],
-            'host' => $_ENV['MYSQL_HOST'],
-            'driver' => 'pdo_mysql',
-        ];
+    private static ?Connection $connection = null;
 
-        return DriverManager::getConnection($connectionParams);
+    public static function getConnection(): ?Connection
+    {
+        if(self::$connection == null) {
+            $connectionParams = [
+                'dbname' => 'news-api',
+                'user' => 'root',
+                'password' => $_ENV['MYSQL_PASSWORD'],
+                'host' => $_ENV['MYSQL_HOST'],
+                'driver' => 'pdo_mysql',
+            ];
+
+            self::$connection = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+        }
+        return self::$connection;
     }
 }
